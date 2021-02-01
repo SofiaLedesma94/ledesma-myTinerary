3cd140f491ecc94596377582b340faad3827bd05
SofiaLedesma111036 <63591158+SofiaLedesma111036@users.noreply.github.com>
2021-01-31 22:11:11 -0300

    commit final sprint3

--- frontend/src/component/Filtro.js
+++ frontend/src/component/Filtro.js
@@ -2,7 +2,7 @@ const Filtro =({info,buscar})=>{
     return(
         
             <input type='text' name='buscador' 
-            placeholder="find your itinerary" value={info} onChange={buscar}></input>
+            placeholder="find your itinerary" value={info} onChange={buscar} autocomplete="off"></input>
     )
 }
 
