3aa5efcbca2b0773893dcb485e4a3df35aba02c5
SofiaLedesma94 <sofii_led@hotmail.com>
2021-02-25 02:17:14 -0300

    m

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose')
 //conexion a BD
-mongoose.connect('mongodb+srv://sofiledesma:paramore@cluster0.xwhfl.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGODB_URI,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true,
