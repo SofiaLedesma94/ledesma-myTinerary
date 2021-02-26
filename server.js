2d12bbe381b40710d32ff4873347e0fdb8137247
SofiaLedesma94 <sofii_led@hotmail.com>
2021-02-26 01:14:48 -0300

    make it better

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(express.json())
 app.use(cors())
 app.use('/', router)
 
-const port=process.env.PORT
+const PORT = process.env.PORT || 4000
 const host=process.env.HOST || '0.0.0.0' 
-app.listen(port,host,console.log('app listening on port 4000'))
+app.listen(PORT,host,console.log(`IS RUNNIG PORT ${PORT}`))
